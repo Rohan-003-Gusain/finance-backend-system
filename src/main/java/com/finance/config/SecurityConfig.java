d10cc6f5fab0f2ac0b4f54e13532836458c514a8
Rohan-003-Gusain <rohangusain95@gmail.com>
2026-04-15 13:37:09 +0530

    update

--- src/main/java/com/finance/config/SecurityConfig.java
+++ src/main/java/com/finance/config/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
             				"/",
             		        "/v3/api-docs/**",
             		        "/swagger-ui/**",
-            		        "/swagger-ui.html"
+            		        "/swagger-ui.html",
+            		        "/ping"
             		).permitAll()
             		
                 .requestMatchers("/", "/login").permitAll()
