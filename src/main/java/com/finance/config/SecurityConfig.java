815975414bcf4418303388e2ff9c18472dd76b77
Rohan-003-Gusain <rohangusain95@gmail.com>
2026-04-15 11:08:38 +0530

    update

--- src/main/java/com/finance/config/SecurityConfig.java
+++ src/main/java/com/finance/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
             		
             		.requestMatchers(
+            				"/",
             		        "/v3/api-docs/**",
             		        "/swagger-ui/**",
             		        "/swagger-ui.html"
